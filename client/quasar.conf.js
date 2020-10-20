cd1c9bb52a40c342c2555ad88b5ebf1905ebd397
icetomcat <icetomcat@gmail.com>
2020-10-20 09:30:03 +0700

    Make main page

--- client/quasar.conf.js
+++ client/quasar.conf.js
@@ -39,7 +39,7 @@ module.exports = configure(function (ctx) {
     extras: [
       // 'ionicons-v4',
       // 'mdi-v5',
-      // 'fontawesome-v5',
+      'fontawesome-v5',
       // 'eva-icons',
       // 'themify',
       // 'line-awesome',
@@ -68,6 +68,10 @@ module.exports = configure(function (ctx) {
 
       // Options below are automatically set depending on the env, set them if you want to override
       // extractCSS: false,
+
+      env: {
+        API: process.env.UPSTREAM
+      },
 
       // https://quasar.dev/quasar-cli/handling-webpack
       extendWebpack (cfg) {
--- client/src/pages/Index.vue
+++ client/src/pages/Index.vue
@@ -1,48 +1,75 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+    <div style="min-width: 250px;">
+      <q-input v-model="filter" label="Search" dense></q-input>
+      <div v-if="!root && !filter" class="fa-2x text-center">
+        <i class="fas fa-spinner fa-spin"></i>
+      </div>
+      <div v-if="!root && filter" class="fa-2x text-center">
+        Not found
+      </div>
+      <div v-if="!!root" style="border: 1px solid #888; padding: 8px;">
+        <TreeNode :root="root" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/ClassComponent.vue'
-import { Vue, Component } from 'vue-property-decorator'
+import { Vue, Component, Provide, Model } from 'vue-property-decorator'
+import { Dictionary, includes, keyBy, uniq, uniqBy, uniqWith } from 'lodash'
+import TreeNode from 'src/components/TreeNode.vue'
+import NestedSet from 'src/store/models/NestedSet'
+import { Collection } from '@vuex-orm/core'
 
 @Component({
-  components: { ExampleComponent }
+  components: {
+    TreeNode
+  }
 })
 export default class PageIndex extends Vue {
-  todos: Todo[] = [
-    {
-      id: 1,
-      content: 'ct1'
-    },
-    {
-      id: 2,
-      content: 'ct2'
-    },
-    {
-      id: 3,
-      content: 'ct3'
-    },
-    {
-      id: 4,
-      content: 'ct4'
-    },
-    {
-      id: 5,
-      content: 'ct5'
-    }
-  ];
+  public filter: string | null = ''
+  public found: number = 0
 
-  meta: Meta = {
-    totalCount: 1200
-  };
+  public get root() {
+    if (this.filter) {
+      var filtered = NestedSet.query().where('title', this.filter).with('children.children').all()
+      const parentsCache: Record<number, NestedSet | null> = keyBy(filtered, 'id')
+      while (filtered?.[0]?.parent_id || filtered.length > 1) {
+        const parents: Collection<NestedSet> = []
+        for (const item of filtered) {
+          item.expanded = true
+          if (item.parent_id) {
+            let parent = parentsCache[item.parent_id]
+            if (!parent) {
+              parent = NestedSet.query().whereId(item.parent_id).first()
+              parentsCache[item.parent_id] = parent
+            }
+            if (parent) {
+              parents.push(parent)
+              parent.children = parent.children ?? []
+              parent.children.push(item)
+              parent.children = uniqBy(parent.children, 'id')
+              parent.expanded = true
+            }
+          } else {
+            item.expanded = true
+            parents.push(item)
+          }
+        }
+        filtered = uniqBy(parents, 'id')
+      }
+      return filtered[0]
+    }
+    const root = NestedSet.query().where('parent_id', null).with('children').first()
+    if (root) {
+      root.expanded = true
+    }
+    return root
+  }
+  
+  async mounted () {
+    await NestedSet.api().get('nested-set')
+  }
 }
 </script>
